a925f7bf556aca02d343a8f844f726fbe665bebf
HP <abhinavchauhan744@gmail.com>
2024-03-17 18:01:33 +0530

    third commit

--- Desktop/E commerce/frontend/src/Components/DescriptionBox/DescriptionBox.jsx
+++ Desktop/E commerce/frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -9,7 +9,8 @@ const DescriptionBox = () => {
         <div className="descriptionbox-nav-box fade">Reviews (122)</div>
       </div>
       <div className="descriptionbox-description">
-        <p></p>
+        <p>An ecommerce website is an online platform where businesses sell products or services to customers over the internet. These websites facilitate transactions between buyers and sellers, allowing customers to browse, select, and purchase items conveniently from the comfort of their own homes or any location with internet access.</p>
+        <p>Ecommerce websites have become increasingly popular due to their convenience, accessibility, and wide selection of products available to consumers worldwide. </p>
       </div>
     </div>
   )
