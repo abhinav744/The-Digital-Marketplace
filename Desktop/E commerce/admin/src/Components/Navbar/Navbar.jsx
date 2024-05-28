e3e0373ed15113e5fe9cc6d838cc93295761f00b
HP <abhinavchauhan744@gmail.com>
2024-05-28 11:12:24 +0530

    sixth commit

--- Desktop/E commerce/admin/src/Components/Navbar/Navbar.jsx
+++ Desktop/E commerce/admin/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Navbar.css'
-import navlogo from '../../assets/nav-logo.svg'
+import logo from '../../assets/logo.png'
 import navProfile from '../../assets/nav-profile.svg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img src={navlogo} alt="" className="nav-logo" />
+      <img src={logo}  alt="" className="" />
+      <h1 >S&C MARKETPLACE</h1>
       <img src={navProfile} alt="" className="nav-profile" />
     </div>
   )
